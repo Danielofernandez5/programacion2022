069c8a3deb9562b66686703f1129fc4934ad7cf2
Daniel Fernandez <104174228+Danielofernandez5@users.noreply.github.com>
2022-05-25 14:07:00 -0300

    actualizando

---
+++ src/areaTriangulo.ts
@@ -0,0 +1,17 @@
+//let base: number = Number(prompt("Ingrese base: "));
+//let altura: number = Number(prompt("Ingrese altura: "));
+let base: number = 1;
+let altura: number = 2;
+
+
+
+function calcularAreaTriangulo(base: number, altura:number):number{
+
+  return (base * altura) / 2;
+}
+for(let cont=0; cont<=7; cont++){
+  console.log(calcularAreaTriangulo(base,altura);
+
+base+=1;
+altura+=2;
+}
