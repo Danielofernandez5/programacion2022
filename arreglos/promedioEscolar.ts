a4926858f9649f6601dcd9f948663c7b983f34ca
Daniel Fernandez <104174228+Danielofernandez5@users.noreply.github.com>
2022-05-31 18:35:02 -0300

    promedio escolar

--- arreglos/promedioEscolar.ts
+++ arreglos/promedioEscolar.ts
@@ -12,12 +12,27 @@ function obtenerPromedioAlumno(indice: number): number {
     3;
   return promedio;
 }
-
-for (let indice: number = 0; indice < 2; indice++) {
-  arrayAlumno[indice] = prompt("Ingrese alumno: ");
-  arrayNota1[indice] = Number(prompt("Ingrese nota 1: "));
-  arrayNota2[indice] = Number(prompt("Ingrese nota 2: "));
-  arrayNota3[indice] = Number(prompt("Ingrese nota 3: "));
+function cargarDatos() {
+  for (let indice: number = 0; indice < 2; indice++) {
+    arrayAlumno[indice] = prompt("Ingrese el alumno: ");
+    arrayNota1[indice] = Number(prompt("Ingrese nota 1: "));
+    arrayNota2[indice] = Number(prompt("Ingrese nota 2: "));
+    arrayNota3[indice] = Number(prompt("Ingrese nota 3: "));
+  }
 }
 
-obtenerPromedioAlumno(1);
+function obtenerPosicion(alumno: string): number {
+  let posicion: number = -1;
+  for (let indice: number = 0; indice < 4; indice++) {
+    if (arrayAlumno[indice] === alumno) {
+      posicion = indice;
+    }
+  }
+  return posicion;
+}
+
+let posicionAlumno: number = obtenerPosicion("B");
+
+cargarDatos();
+let alumno: string = prompt("Ingrese alumno: ");
+console.log("El promedio del alumno "+alumno+ " es :" obtenerPromedioAlumno(obtenerPosicion(alumno)));
